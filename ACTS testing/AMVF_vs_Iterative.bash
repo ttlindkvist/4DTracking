0acf25f5eadadfe7f80002be5443eaa4b6c3d08f
ttlindkvist <thomas.toft@hotmail.com>
2022-08-03 15:20:18 +0200

    local pu and vtx time

--- ACTS testing/AMVF_vs_Iterative.bash
+++ ACTS testing/AMVF_vs_Iterative.bash
@@ -1,6 +1,6 @@
 ../../acts/install/bin/AMVFvsIterativeVertexFinder \
-    -n 10 \
-    --gen-npileup 50 \
+    -n 100 \
+    --gen-npileup 150 \
     --output-dir test/ \
     --loglevel 1 \
     > amvf_vs_iterative_output.txt
